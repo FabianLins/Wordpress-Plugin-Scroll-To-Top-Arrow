b07b91e1683d37ea116db7aa2f363d54ae3cf7cf
Fabian Lins <local@local.local>
2024-11-14 23:26:41 +0200

    sanitizing input, modal box for name, error alerts

--- script/admin.js
+++ script/admin.js
@@ -1,4 +1,8 @@
 function linsScrollTopSavePreset() {
+
+    const presetName = document.querySelector('[name="lins_scroll_preset_name"]');
+    console.log(presetName);
+
     const scrollArrowFill = document.querySelector('[name="lins_scroll_arrow_fill"]');
     //console.log(scrollArrowFill);
 
@@ -63,6 +67,7 @@ function linsScrollTopSavePreset() {
     //console.log(scrollBgOpacity);
 
     myPreset = {
+        presetName: presetName.value,
         scrollArrowFill: scrollArrowFill.value,
         scrollArrowOpacity: scrollArrowOpacity.value,
         scrollArrowBg: scrollArrowBg.value,
@@ -91,11 +96,51 @@ function linsScrollTopSavePreset() {
         url: `${window.location.origin}/wp-admin/admin-ajax.php`,
         dataType: 'json',
         data: {
-            action: "save_preset",
+            action: 'save_preset',
             ajax_data: myPreset
         },
-        complete: function (response) {
-            alert('Preset saved successfully');
+        complete: function (errors) {
+            if (errors.responseText != 0) {
+                //console.log(errors);
+                let fixedResponseText;
+                if (errors.responseText.substr(errors.responseText.length - 1, errors.responseText.length) == '0') {
+                    fixedResponseText = (errors.responseText).substr(0, errors.responseText.length - 1);
+                }
+                else {
+                    fixedResponseText = errors.responseText;
+                }
+                fixedResponseText = JSON.parse(fixedResponseText);
+                const errorAlerts = (Object.values(fixedResponseText));
+                let errorAlertsSpaces = '';
+                for (let i = 0; i < errorAlerts.length; i++) {
+                    if (i !== errorAlerts.length - 1) {
+                        errorAlertsSpaces += `${errorAlerts[i]}, `;
+                    }
+                    else {
+                        errorAlertsSpaces += errorAlerts[i];
+                    }
+                }
+                document.querySelector('.alert-boxes').innerHTML +=
+                    `<div id="setting-error-settings_updated" class="notice notice-error settings-error is-dismissible">
+                        <p>
+                            <strong>
+                                ${errorAlertsSpaces}
+                            </strong>
+                        </p>
+                    </div>`;
+            }
+            else {
+                document.querySelector('.alert-boxes').innerHTML += `<div id="setting-error-settings_updated" class="notice notice-success settings-error is-dismissible">
+                <p>
+                    <strong>
+                        Preset saved successfully! Do you want to  apply the settings to the page? Click "Save Changes" at the bottom!
+                    </strong>
+                </p>
+            </div>`;
+
+            }
+            linsScrollTopCloseModal();
+            window.scrollTo({ top: 0, behavior: 'smooth' });
         }
     });
 }
