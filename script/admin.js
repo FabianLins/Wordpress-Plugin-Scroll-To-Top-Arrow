c499ac77f6cf4383244878d278e82cbd95d27d31
Fabian Lins <local@local.local>
2024-11-19 00:03:12 +0200

    update preset

--- script/admin.js
+++ script/admin.js
@@ -70,6 +70,151 @@ function linsScrollReloadPresetSelect() {
     });
 }
 
+function linsScrollUpdatePreset() {
+    const scrollArrowFill = document.querySelector('[name="lins_scroll_arrow_fill"]');
+    //console.log(scrollArrowFill);
+
+    const scrollArrowOpacity = document.querySelector('[name="lins_scroll_arrow_opacity"]');
+    //console.log(scrollArrowOpacity);
+
+    const scrollArrowBg = document.querySelector('[name="lins_scroll_arrow_color"]');
+    //console.log(scrollArrowBg);
+
+    const scrollArrowOpacityHover = document.querySelector('[name="lins_scroll_arrow_opacity_hover"]');
+    //console.log(scrollArrowOpacityHover);
+
+    const scrollArrowBgHover = document.querySelector('[name="lins_scroll_arrow_color_hover"]');
+    //console.log(scrollArrowBgHover);
+
+    const scrollArrowBgSize = document.querySelector('[name="lins_scroll_bg_size"]');
+    //console.log(scrollArrowBgSize);
+
+    const scrollArrowBgSizeLg = document.querySelector('[name="lins_scroll_bg_size_lg"]');
+    //console.log(scrollArrowBgSizeLg);
+
+    const scrollArrowBgSizeMd = document.querySelector('[name="lins_scroll_bg_size_md"]');
+    //console.log(scrollArrowBgSizeMd);
+
+    const scrollArrowBgSizeSm = document.querySelector('[name="lins_scroll_bg_size_sm"]');
+    //console.log(scrollArrowBgSizeSm);
+
+    const scrollArrowSize = document.querySelector('[name="lins_scroll_arrow_size"]');
+    //console.log(scrollArrowSize);
+
+    const scrollArrowMargin = document.querySelector('[name="lins_scroll_arrow_margin"]');
+    //console.log(scrollArrowMargin);
+
+    const scrollArrowMarginLg = document.querySelector('[name="lins_scroll_arrow_margin_lg"]');
+    //console.log(scrollArrowMarginLg);
+
+    const scrollArrowMarginMd = document.querySelector('[name="lins_scroll_arrow_margin_md"]');
+    //console.log(scrollArrowMarginMd);
+
+    const scrollArrowMarginSm = document.querySelector('[name="lins_scroll_arrow_margin_sm"]');
+    //console.log(scrollArrowMarginSm);
+
+    const scrollArrowTranslate = document.querySelector('[name="lins_scroll_arrow_translate"]');
+    //console.log(scrollArrowTranslate);
+
+    const scrollBgHeight = document.querySelector('[name="lins_scroll_bg_height"]');
+    //console.log(scrollBgHeight);
+
+    const scrollBgHeightLg = document.querySelector('[name="lins_scroll_bg_height_lg"]');
+    //console.log(scrollBgHeightLg);
+
+    const scrollBgHeightMd = document.querySelector('[name="lins_scroll_bg_height_md"]');
+    //console.log(scrollBgHeightMd);
+
+    const scrollBgHeightSm = document.querySelector('[name="lins_scroll_bg_height_sm"]');
+    //console.log(scrollBgHeightSm);
+
+    const scrollBgColor = document.querySelector('[name="lins_scroll_bg_color"]');
+    //console.log(scrollBgColor);
+
+    const scrollBgOpacity = document.querySelector('[name="lins_scroll_bg_opacity"]');
+    //console.log(scrollBgOpacity);
+
+    myPreset = {
+        uuid: loadedUuid,
+        scrollArrowFill: scrollArrowFill.value,
+        scrollArrowOpacity: scrollArrowOpacity.value,
+        scrollArrowBg: scrollArrowBg.value,
+        scrollArrowOpacityHover: scrollArrowOpacityHover.value,
+        scrollArrowBgHover: scrollArrowBgHover.value,
+        scrollArrowBgSize: scrollArrowBgSize.value,
+        scrollArrowBgSizeLg: scrollArrowBgSizeLg.value,
+        scrollArrowBgSizeMd: scrollArrowBgSizeMd.value,
+        scrollArrowBgSizeSm: scrollArrowBgSizeSm.value,
+        scrollArrowSize: scrollArrowSize.value,
+        scrollArrowMargin: scrollArrowMargin.value,
+        scrollArrowMarginLg: scrollArrowMarginLg.value,
+        scrollArrowMarginMd: scrollArrowMarginMd.value,
+        scrollArrowMarginSm: scrollArrowMarginSm.value,
+        scrollArrowTranslate: scrollArrowTranslate.value,
+        scrollBgHeight: scrollBgHeight.value,
+        scrollBgHeightLg: scrollBgHeightLg.value,
+        scrollBgHeightMd: scrollBgHeightMd.value,
+        scrollBgHeightSm: scrollBgHeightSm.value,
+        scrollBgColor: scrollBgColor.value,
+        scrollBgOpacity: scrollBgOpacity.value
+    };
+
+    jQuery.ajax({
+        type: 'post',
+        url: `${window.location.origin}/wp-admin/admin-ajax.php`,
+        dataType: 'json',
+        data: {
+            action: 'update_preset',
+            ajax_data: myPreset
+        },
+        complete: function (errors) {
+            if (errors.responseText) {
+                //console.log(errors);
+                errors.responseText = JSON.parse(errors.responseText);
+                const errorAlerts = (Object.values(errors.responseText));
+                let errorAlertsSpaces = '';
+                for (let i = 0; i < errorAlerts.length; i++) {
+                    if (i !== errorAlerts.length - 1) {
+                        errorAlertsSpaces += `${errorAlerts[i]}, `;
+                    }
+                    else {
+                        errorAlertsSpaces += errorAlerts[i];
+                    }
+                }
+                const alerts = document.querySelectorAll('.lins-scroll-arrow-alert');
+                alerts.forEach(currAlert => {
+                    currAlert.classList.add('js-hide-alert');
+                });
+                document.querySelector('.alert-boxes').innerHTML +=
+                    `<div id="setting-error-settings_updated" class="notice notice-error settings-error lins-scroll-arrow-alert">
+                        <p>
+                            <strong>
+                                The field(s): ${errorAlertsSpaces} contain(s) errors. Preset couldn't be updated.
+                            </strong>
+                        </p>
+                    </div>`;
+            }
+            else {
+                const alerts = document.querySelectorAll('.lins-scroll-arrow-alert');
+                alerts.forEach(currAlert => {
+                    currAlert.classList.add('js-hide-alert');
+                });
+                document.querySelector('.alert-boxes').innerHTML += `<div id="setting-error-settings_updated" class="notice notice-success settings-error lins-scroll-arrow-alert">
+                                                                        <p>
+                                                                            <strong>
+                                                                                Preset updated successfully! To aply the preset, click on <span class="js-save-changes-to-page link-look">"Save Changes" at the bottom of the page or here</span>.
+                                                                            </strong>
+                                                                        </p>
+                                                                    </div>`;
+                linsScrollPresetApply();
+                linsScrollReloadPresetSelect();
+                linsScrollReloadRemoveSelect();
+            }
+        }
+    });
+
+}
+
 function linsScrollEditPreset() {
     const editPresetName = document.querySelector('[name="lins_scroll_preset_edit"]').value;
     newName = {
@@ -419,6 +564,8 @@ function linsScrollLoadPreset() {
                     </div>`;
                 linsScrollPresetApply();
                 document.querySelector('.edit-preset-btn').classList.add("js-show-btn");
+                document.querySelector('.update-preset-btn').classList.add("js-show-btn");
+
             } catch (error) {
                 console.error(error);
                 const alerts = document.querySelectorAll('.lins-scroll-arrow-alert');
